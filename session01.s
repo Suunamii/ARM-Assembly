a0baa35c9680c32d6edc8a1a6b67971de4089327
Suss <83242763+Suunamii@users.noreply.github.com>
2024-12-28 19:54:51 +0100

    branch

--- session01.s
+++ session01.s
@@ -1,6 +1,13 @@
 .global main /* main must be global, for C runtime to be able to call it*/
 .func main /* main is a function, specifies between code and data*/
+.func add2
 
 main:
-  mov r0, #2 /*put a 2 inside the register r0, r0 contains the error code of function main*/
-  bx lr  /* return from main*/
+  mov r0, #1 /*put a 2 inside the register r0, r0 contains the error code of function main*/
+  mov r1, #3
+  bl add2
+  mov r3, #4
+
+add2:
+  add r2, r0, r1
+  bx lr  /* return to caller (returning to main)*/
